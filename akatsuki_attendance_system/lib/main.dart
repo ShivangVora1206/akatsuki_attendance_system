1cccc8f5a1c0db36162b9b7104a33f931c7d2a95
Hitesh Jadhav <jadhavhitesh207@gmail.com>
2022-05-04 04:36:55 +0530

    Hitesh change the ui

--- akatsuki_attendance_system/lib/main.dart
+++ akatsuki_attendance_system/lib/main.dart
@@ -10,6 +10,7 @@ import 'package:http/http.dart' as http;
 import 'function.dart';
 void main() {
   runApp( MaterialApp(
+    debugShowCheckedModeBanner: false,
     home:AnimatedSplashScreen(
       splash: Container(
         child: Center(
@@ -43,6 +44,7 @@ class Baseapp extends StatefulWidget{
 }
 
 class _BaseappState extends State<Baseapp> {
+  String greeting = "";
   late final String? PRN;
   // ignore: non_constant_identifier_names, unused_element
   _BaseappState({String? key, required this.PRN}) : super();
@@ -53,6 +55,7 @@ class _BaseappState extends State<Baseapp> {
   List subjectNames = [];
   List subjectAttendances = [];
   var data;
+  var data2;
   var output = 0;
     int n = 0;
   @override
@@ -64,17 +67,17 @@ class _BaseappState extends State<Baseapp> {
     // final List<int> _subjectAttendances = subjectAttendances.cast<int>();
     // final List<String> _subjectNames = subjectNames.cast<String>();
 
-     List<int?>  _subjectAttendances = subjectAttendances.cast<int?>();
-     List<String?> _subjectNames = subjectNames.cast<String?>();
+     // List<int?>  _subjectAttendances = subjectAttendances.cast<int?>();
+     // List<String?> _subjectNames = subjectNames.cast<String?>();
 
 
 
-    // final List<int> _subjectAttendances = <int>[1, 2, 3, 4, 5];
-    // final List<String> _subjectNames = <String>['sub1',
-    //   'sub2',
-    //   'sub3',
-    //   'sub4',
-    //   'sub5'];
+    final List<int> _subjectAttendances = <int>[1, 2, 3, 4, 5];
+    final List<String> _subjectNames = <String>['sub1',
+      'sub2',
+      'sub3',
+      'sub4',
+      'sub5'];
 
 
     return Container(
@@ -82,8 +85,8 @@ class _BaseappState extends State<Baseapp> {
         gradient: RadialGradient(
             radius: 1,
             colors: [
-              Colors.white,
-              Colors.white
+              Colors.lightBlue,
+              Colors.lightGreenAccent,
             ]
         ),
       ),
@@ -93,13 +96,19 @@ class _BaseappState extends State<Baseapp> {
             onPressed: () async {
               data = await fetchdata("http://10.0.0.3:8000/getStudentData/$PRN");
               var decoded = jsonDecode(data);
+              // data2 = await fetchdata("http://127.0.0.1:5000/u");
+              // var decoded2 = jsonDecode(data2);
               print("pressed");
               setState(() {
+                // greeting = decoded2['greetings'];
+                // print(greeting);
+                // print(greeting);
+                // print(greeting);
                 subjectNames.clear();
                 subjectAttendances.clear();
 
                 studentName = decoded['studentName'];
-                print(studentName);
+                // print(studentName);
                 studentPRN = decoded['studentPRN'].toString();
                 studentRollNo = decoded['studentRollNo'].toString();
                 packetSize = int.parse(decoded['packetSize']);
@@ -117,18 +126,18 @@ class _BaseappState extends State<Baseapp> {
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                       colors: [
+                        Colors.blue,
                         Colors.red,
-                        Colors.redAccent
                       ]
                   ),
                 )
             ),
             centerTitle: true,
-            title: Text("Akatsuki Attendance System"),
+            title: Icon(Icons.home),
           ),
           backgroundColor: Colors.transparent,
           body: ListView.separated(
-            padding: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(3),
             itemCount: _subjectAttendances.length,
             itemBuilder: (BuildContext context, int index) {
               return Container(
@@ -183,14 +192,14 @@ class _inputPRNPage extends State<inputPRNPage> {
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                     colors: [
-                      Colors.red,
+                      Colors.blue,
                       Colors.redAccent
                     ]
                 ),
               )
           ),
           centerTitle: true,
-          title: Text("Akatsuki Attendance System"),
+          title: Text("Akatsuki Attendance Systemm"),
         ),
         body: Padding(
           padding: const EdgeInsets.all(20),
@@ -203,10 +212,10 @@ class _inputPRNPage extends State<inputPRNPage> {
                 decoration: InputDecoration(
                 labelText: 'Enter PRN Number',
                 labelStyle: TextStyle(
-                    color: Colors.black
+                    color: Colors.grey,
                 ),
                 enabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(width: 3, color: Colors.red),
+                                borderSide: const BorderSide(width: 3, color: Colors.blue),
                                 borderRadius: BorderRadius.circular(15),
                               ),
                 focusedBorder: OutlineInputBorder(
@@ -217,7 +226,7 @@ class _inputPRNPage extends State<inputPRNPage> {
                 controller: _textController,
 
               ),
-              SizedBox(height: 25),
+              SizedBox(height: 10),
               ElevatedButton(
 
                 style: ButtonStyle(
@@ -225,15 +234,15 @@ class _inputPRNPage extends State<inputPRNPage> {
                         (Set<MaterialState> states) {
                       if (states.contains(MaterialState.pressed))
                         return Colors.black;
-                      return Colors.red; // Use the component's default.
+                      return Colors.lightBlue; // Use the component's default.
                     },
                   ),
                 ),
-                  onPressed: (){
+                  onPressed: ()async {
                     captureInput();
                     print(_result);
                     Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => Baseapp(PRN: _result.toString())
+                        builder: (context) => Baseapp(PRN: _result)
                     )
                     );
                   },
